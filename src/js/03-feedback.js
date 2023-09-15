a1d9d8176167e46b27acc31b0bc3ae99cee509f0
Elostay <nastia60203@gmail.com>
2023-09-15 10:40:49 +0300

    2

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -10,11 +10,26 @@ const STORAGE_KEY = 'feedback-form-state';
 
 refs.form.addEventListener('submit', onSubmit);
 refs.form.addEventListener('input', throttle(onInput, 500));
-const inputValue = {};
+
 populateTextarea();
+
+let inputValue = {
+  email: '',
+  message: '',
+};
+
 function onSubmit(event) {
   event.preventDefault();
-  event.currentTarget.reset();
+  refs.form.reset();
+  const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (savedMessage) {
+    inputValue = savedMessage;
+  } else {
+    inputValue = {
+      email: '',
+      message: '',
+    };
+  }
   localStorage.removeItem(STORAGE_KEY);
   console.log(inputValue);
 }
@@ -24,7 +39,7 @@ function onInput(event) {
   localStorage.setItem(STORAGE_KEY, JSON.stringify(inputValue));
 }
 
-function populateTextarea(event) {
+function populateTextarea() {
   const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
   if (savedMessage) {
     refs.input.value = savedMessage.email;
